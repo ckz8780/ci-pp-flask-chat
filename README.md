80ee294a607d6574c66b3c402b1acb17f91d8c84
Chris Zielinski <ckz8780@gmail.com>
2018-05-27 23:13:28 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome!
+
+This is a simple chat application written in Flask. It is part of the Practical Python module for CodeInstitute.
+
+#### Live Version: 
+
+There is a live version available at http://ci-pp-flask-chat.herokuapp.com
---
+++ run.py
@@ -0,0 +1,59 @@
+import os
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+
+def write_to_file(filename, data):
+    """Handle the process of writing data to a file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+
+def add_messages(username, message):
+    """Add messages to the `messages` text file"""
+    write_to_file("data/messages.txt", "({0}) {1} - {2}\n".format(
+            datetime.now().strftime("%H:%M:%S"),
+            username.title(),
+            message))
+
+
+def get_all_messages():
+    """Get all of the messages and separate them by a `br`"""
+    messages = []
+    with open("data/messages.txt", "r") as chat_messages:
+        messages = chat_messages.readlines()
+    return messages
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    """Main page with instructions"""
+    
+    # Handle POST request
+    if request.method == "POST":
+        write_to_file("data/users.txt", request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    return render_template("index.html")
+
+
+@app.route('/<username>', methods=["GET", "POST"])
+def user(username):
+    """Display chat messages"""
+    
+    if request.method == "POST":
+        add_messages(username, request.form["message"] + "\n")
+    
+    messages = get_all_messages()
+    return render_template("chat.html",
+                            username=username, chat_messages=messages)
+
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect(username)
+
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
